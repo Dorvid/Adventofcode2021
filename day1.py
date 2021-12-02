479de455f4a407504a58089718da43e2fd540b7d
davfr261 <davfr261@tlvm-4-1-1.ad.liu.se>
2021-12-02 12:48:47 +0100

    day1 finished

---
+++ day1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import string
+
+
+tempfile = open("inputDay1.txt")
+input = tempfile.readlines()
+data = [[int(val) for val in line.split()]for line in input]
+cnt = 0
+
+print(data)
+for i, j in enumerate(data[:-1]):
+    if j < data[i+1]:
+        cnt += 1
+
+print("Total increases: " + str(cnt))
